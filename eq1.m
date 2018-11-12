06f28e818872668292476d6c7c3414830d590759
AlexandreHnf <h.alexandre99@hotmail.com>
2018-11-12 15:27:13 +0100

    system 1

---
+++ eq1.m
@@ -0,0 +1,59 @@
+A = [1 3; 0 0];
+
+% le système
+ode_sys = @(t,x) [A(1,1)*x(1) + A(1,2)*x(2);A(2,1)*x(1) + A(2,2)*x(2)];
+
+[t,x]=ode23(ode_sys, [0,10], [0.5,0]);
+%plot(t,x)
+
+% calcul des valeurs propres
+[V,L] = eig(A)
+
+% === dessin des vecteurs propres ===
+line_range = -1.5:.1:1.5;
+line_1 = (V(2,1)/V(1,1)) * line_range;
+line_2 = (V(2,2)/V(1,2)) * line_range;
+hold on;
+%plot(line_range,line_1,"linewidth",10);
+%plot(line_range,line_2,"linewidth",10);
+%quiver([0;0],[0;0],V(1,:),V(2,:),"linewidth",10,"color","k")
+
+
+% === dessin vecteurs vitesse ===
+point_sequence = [[1,1];[-1,-1];[-1,1];[1,-1]]';
+velocity_sequence = [];
+for point_vec = point_sequence # Column-wise iteration
+    velocity_vec = A*point_vec
+    velocity_norm = velocity_vec/norm(velocity_vec);
+    velocity_sequence = [velocity_sequence,velocity_norm];
+endfor
+
+% affichage
+hold on;
+%quiver(point_sequence(1,:),point_sequence(2,:),velocity_sequence(1,:),velocity_sequence(2,:),0.2,"linewidth",5,"color","k")
+%plot(point_sequence(1,:),point_sequence(2,:),"ok")
+%plot(line_range,line_1,"linestyle",":","color","k");
+%plot(line_range,line_2,"linestyle",":","color","k");
+
+
+%   === portrait de phase ===
+
+x1range=-1.5:.1:1.5;
+x2range=-1.5:.1:1.5;
+[x1,x2] = meshgrid(x1range, x2range);
+
+# Définit le système à afficher (basé sur la matrice A)
+x1p = A(1,1)*x1+A(1,2)*x2;
+x2p = A(2,1)*x1+A(2,2)*x2;
+
+# Normalise les valeurs pour l'affichage
+arrow=sqrt(x1p.^2+x2p.^2);
+
+# Création du portrait de phase
+hold on;
+quiver(x1,x2,x1p./arrow,x2p./arrow,0.5);
+# Line plot
+plot(line_range,line_1,"linewidth",10,"color","k");
+plot(line_range,line_2,"linewidth",10,"color","k");
+grid on;
+axis tight;
